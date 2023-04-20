987dafd90244f070143ac32819db27553e54dc34
daisy.omoikirodah <thesmexxiichic@gmail.com>
2023-04-20 02:24:35 -0400

    done

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Newtonsoft.Json.Linq;
+
+bool cont = true;
+
+do
+{
+    Quotes.KanyeQuote();
+    Quotes.RonQuote();
+
+    Console.WriteLine("Continue? Y or N7?");
+    var response = Console.ReadLine().ToLower();
+
+    cont = (response == "n") ? false : true;
+
+} while (cont);
+    
---
+++ KanyeWest/Quotes.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+
+public class Quotes
+{
+    public Quotes()
+    {
+
+    }
+
+    public static void KanyeQuote()
+    {
+        var client = new HttpClient();
+
+        //Kanye
+        var kanyeURL = "https://api.kanye.rest/";
+        var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+        var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+        Console.WriteLine($"{kanyeQuote}\n");
+    }
+
+    public static void RonQuote()
+    {
+        var client = new HttpClient();
+
+        //Ron Swanson
+        var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+        var ronResponse = client.GetStringAsync(ronURL).Result;
+        var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+        Console.WriteLine(ronQuote);
+    }
+}
+    
+  
